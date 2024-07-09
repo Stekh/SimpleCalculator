cf0358dc21283b8247e895594844c385ff10b5be
Stekh <bartoszstec@onet.pl>
2024-07-09 20:14:21 +0200

    init commit + html

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,12 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri;
+
+
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
