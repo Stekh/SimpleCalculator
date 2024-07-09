04e23ad95c9908ab31e71eab0286bd076722e94e
Stekh <bartoszstec@onet.pl>
2024-07-09 20:20:12 +0200

    add state struct

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,28 @@
 
 use tauri;
 
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    SQRT,
+    Flip,
+    Del,
+    Clear,
+    Calc,
+    Nop,
+}
 
+struct Calculator {
+    num1: f64,
+    num2: f64,
+    op: Operation,
+}
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(Calculator {num1: 0.0, num2: 0.0, op: Operation::Nop})
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
